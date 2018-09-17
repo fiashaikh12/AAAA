18822b16f50cbcd1235e01cd489bda3309f41b45
fiashaikh12 <firoz.anis.shaikh@gmail.com>
2018-09-18 00:04:06 +0530

    hi

--- MerchantDistributorService_API/Controllers/UserController.cs
+++ MerchantDistributorService_API/Controllers/UserController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 
+/*Developed by Arif & Firoz*/
 namespace MerchantDistributorService_API.Controllers
 {
     public class UserController : ApiController
