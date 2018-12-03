00c661b318e32d5e798b65d587ab4bfd8c35c9e5
fiashaikh12 <firoz.anis.shaikh@gmail.com>
2018-12-03 08:48:35 +0530

    new chaneg

---
+++ BusinessLogicLayer/Repository/MessageRepository.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using DataAccessLayer;
+using Entities;
+using Interface;
+namespace Repository
+{
+    public class MessageRepository : IMessage
+    {
+        public ServiceRes AllMessages(Messages messages)
+        {
+            ServiceRes<List<Messages>> serviceRes = new ServiceRes<List<Messages>>();
+            try
+            {
+                SqlParameter[] sqlParameter = new SqlParameter[2];
+                sqlParameter[0] = new SqlParameter { ParameterName = "@senderId", Value = messages.SenderId };
+                sqlParameter[1] = new SqlParameter { ParameterName = "@flag", Value = "G" };
+                var dataTable = SqlHelper.GetTableFromSP("Usp_MessageMaster", sqlParameter);
+                if (dataTable!=null || dataTable.Rows.Count >0)
+                {
+                    serviceRes.Data = dataTable.AsEnumerable().Select(x => new Messages {
+                        MessageContent=x.Field<string>("Msg_Description"),
+                        RecieverId=x.Field<int>("Recipent_Id"),
+                        SenderId=x.Field<int>("Sender_Id"),
+                        IsRead=x.Field<bool>("Read_Flag"),
+                        MessageDate=x.Field<DateTime>("Created_On")
+                    }).ToList();
+                    serviceRes.IsSuccess = true;
+                    serviceRes.ReturnCode = "200";
+                    serviceRes.ReturnMsg = "Success";
+                }
+                else
+                {
+                    serviceRes.IsSuccess = false;
+                    serviceRes.ReturnCode = "400";
+                    serviceRes.ReturnMsg = "Failed";
+                }
+            }
+            catch (Exception ex)
+            {
+                serviceRes.IsSuccess = false;
+                serviceRes.ReturnCode = "500";
+                serviceRes.ReturnMsg = "Exception occured";
+                LogManager.WriteLog(ex);
+            }
+            return serviceRes;
+        }
+
+        public ServiceRes Send(Messages messages)
+        {
+            ServiceRes serviceRes = new ServiceRes();
+            try {
+                SqlParameter[] sqlParameter = new SqlParameter[4];
+                sqlParameter[0] = new SqlParameter { ParameterName = "@senderId", Value = messages.SenderId };
+                sqlParameter[1] = new SqlParameter { ParameterName = "@reciId", Value = messages.RecieverId };
+                sqlParameter[2] = new SqlParameter { ParameterName = "@msgDiscription", Value = messages.MessageContent };
+                sqlParameter[3] = new SqlParameter { ParameterName = "@flag", Value ="S" };
+                int returnValue = SqlHelper.ExecuteNonQuery("Usp_MessageMaster", sqlParameter);
+                if(returnValue > 0)
+                {
+                    serviceRes.IsSuccess = true;
+                    serviceRes.ReturnCode = "200";
+                    serviceRes.ReturnMsg = "Success";
+                }
+                else
+                {
+                    serviceRes.IsSuccess = false;
+                    serviceRes.ReturnCode = "400";
+                    serviceRes.ReturnMsg = "Failed";
+                }
+            }
+            catch(Exception ex)
+            {
+                serviceRes.IsSuccess = false;
+                serviceRes.ReturnCode = "500";
+                serviceRes.ReturnMsg = "Exception occured";
+                LogManager.WriteLog(ex);
+            }
+            return serviceRes;
+        }
+    }
+}
---
+++ MerchantDistributorService_API/Controllers/MessageController.cs
@@ -0,0 +1,31 @@
+﻿using Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Entities;
+namespace MerchantDistributorService_API.Controllers
+{
+    public class MessageController : ApiController
+    {
+        private readonly IMessage _message;
+        public MessageController(IMessage message)
+        {
+            this._message = message;
+        }
+
+        [HttpPost]
+        public IHttpActionResult Send(Messages request)
+        {
+            return Ok(this._message.Send(request));
+        }
+
+        [HttpPost]
+        public IHttpActionResult GetMessages(Messages request)
+        {
+            return Ok(this._message.AllMessages(request));
+        }
+    }
+}
